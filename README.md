978933e8872cafd8a80a8d6e95f4cfcd7a156205
Adam Leis <aleis@iti4dmv.com>
2025-03-03 06:47:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue 3 + Vite
+
+This is a test project to illustrate a duplicate text problem I'm having. Maybe the community will have some ideas to how to solve this issue.
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const loaded = ref(false);
+// pretend this is an async func grabbing config details for the app (e.g. different branding & verbiage based on biz client details)
+setTimeout(() => loaded.value = true, 1000);
+</script>
+
+<template>
+  <div aria-live="polite">
+    <div v-if="loaded">
+      <label for="foo">Foo Input</label>
+      <input type="text" id="foo">
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
